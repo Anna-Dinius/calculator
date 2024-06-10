a110a21b3752bad4f8ea9e3efe409f7bb3f16bb4
Anna-Dinius <156371391+Anna-Dinius@users.noreply.github.com>
2024-06-10 15:40:11 -0400

    refactored code & fixed bugs

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { evaluate } from 'mathjs';
+
 @Component({
   selector: 'calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit{
+  // declaring variables
   calculatedValue!: string;
   previousButton: string = 'none';
-  operators: string[] = [ '+', '-', '*', '/']
   clickedButton: string | null = null;
+
+  cvHasPeriod!: boolean;
   buttonValIsCharacter!: boolean; 
   buttonValIsNumber!: boolean; 
   buttonValIsOperator!: boolean; 
-  prevButtonValIsOperator!: boolean; 
+  prevButtonValIsOperator!: boolean;
+
+  operators: string[] = [ '+', '-', '*', '/' ];
   buttonValues: any[] = [ 
     [7, 8, 9, '/'],
     [4, 5, 6, 'x'],
@@ -22,12 +28,13 @@ export class CalculatorComponent implements OnInit{
   ];
 
   ngOnInit(): void {
-    this.handleClear();
+    this.handleClear('none');
   }
 
+  // functions
   determineBackgroundColor(value: number | string):string {
     value = value.toString();
-    if (this.clickedButton === value) {
+    if (this.clickedButton == value) {
       return '#ccc';
     }
     return '';
@@ -44,84 +51,209 @@ export class CalculatorComponent implements OnInit{
     return (this.operators.includes(value));
   }
 
-  handleClear() {
+  cvHasOperator(): boolean {
+    for (let  i = 0; i < this.operators.length; i++) {
+      if (this.calculatedValue.toString().includes(this.operators[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkValidity(): boolean {
+    let operatorPosition: number = 0;
+    let periodPosition: number = 0;
+    for (let i = 0; i < this.calculatedValue.length; i++){
+      if (this.isOperator(this.calculatedValue.charAt(i)))  {
+        operatorPosition = i;
+      }
+      else if (this.calculatedValue.charAt(i) === '.') {
+        periodPosition = i;
+      }
+    }
+
+    if (operatorPosition > periodPosition) { return true; }
+    return false;
+  }
+
+  handleClear(buttonValue: string) {
     this.calculatedValue = '0';
-    this.previousButton = 'none';
+    this.previousButton = buttonValue;
   }
 
   handleBackspace() {
-    if (this.previousButton !== '=') {
+    if (this.calculatedValue.length > 1) {
       this.calculatedValue = this.calculatedValue.substring(0, this.calculatedValue.length - 1);
-      if (this.calculatedValue === '') {
-        this.handleClear();
-      }
+      this.previousButton = this.calculatedValue.substring(this.calculatedValue.length - 1, this.calculatedValue.length);
+    }
+    else {
+      this.calculatedValue = '0';
     }
   }
 
   handleClick(buttonValue: number | string) {
-    if (typeof buttonValue === 'number') {
-      buttonValue = buttonValue.toString();
-    }
-    console.log('buttonValue: ' + buttonValue);
-
-    this.clickedButton = buttonValue;
-    setTimeout(() => {
-      this.clickedButton = null;
-    }, 200);
-
     if (buttonValue === 'x') {
       buttonValue = '*';
     }
 
-    this.buttonValIsCharacter = isNaN(parseInt(buttonValue));
-    this.buttonValIsNumber = !this.buttonValIsCharacter;
-    this.buttonValIsOperator = this.isOperator(buttonValue);
+    //console.log('buttonValue: ' + buttonValue);
+
+    // setting boolean variables
+    this.buttonValIsOperator = this.isOperator(buttonValue.toString());
     this.prevButtonValIsOperator = this.isOperator(this.previousButton);
+    this.cvHasPeriod = this.calculatedValue.toString().includes('.');
     
-    if ((this.previousButton === '=' && this.buttonValIsNumber) || this.calculatedValue === 'Error') {
+    // changing background color of button
+    this.clickedButton = buttonValue.toString();
+    setTimeout(() => {
+      this.clickedButton = null;
+    }, 200);
+
+    let buttonValueString: string = buttonValue.toString();
+
+    // setting calculatedValue
+    if (this.calculatedValue === 'Error') {
+      if (typeof buttonValue === 'number') {
+        this.calculatedValue = buttonValueString;
+        this.previousButton = buttonValueString;
+      }
+      else if (buttonValue === '.') {
+        this.calculatedValue = '0.';
+        this.previousButton = '.';
+      }
+      else {
+        this.calculatedValue = 'Error';
+        this.previousButton = 'none';
+      }
+    }
+    else {
+      if (typeof buttonValue === 'number') {
+        this.buttonValIsNumber = true;
+        this.buttonValIsCharacter = false;
+
+        this.handleNumber(buttonValueString);
+      }
+      else if (typeof buttonValue === 'string') {
+        this.buttonValIsNumber = false;
+        this.buttonValIsCharacter = true;
+
+        this.handleCharacter(buttonValueString);
+      }
+      else {
+        this.calculatedValue = 'Error';
+        this.previousButton = 'none';
+        
+        //console.log('Error w/o handling buttonValue');
+      }
+    }
+  }
+
+  handleNumber(buttonValue: string) {
+    if (buttonValue == '0') {
+      if (this.calculatedValue == '0') {
+        this.handleClear(buttonValue);
+      }
+      else {
+        this.calculatedValue += '0';
+      }
+    }
+    else if (this.calculatedValue == '0' || this.previousButton === '=') {
       this.calculatedValue = buttonValue;
     }
-    else if ((this.prevButtonValIsOperator && this.buttonValIsOperator)) {
+    else {
+      this.calculatedValue += buttonValue;
+    }
+
+    this.previousButton = buttonValue;
+    //console.log('handleNumber()');
+  }
+
+  handleCharacter(buttonValue: string) {
+    if (this.calculatedValue == '0' && buttonValue !== '=') {
+      this.calculatedValue += buttonValue;
+      this.previousButton = buttonValue;
+    }
+    else {
+      if (this.buttonValIsOperator) {
+        this.handleOperator(buttonValue);
+      }
+      else if (buttonValue === '.') {
+        this.handlePeriod();
+      }
+      else if (buttonValue === '=') {
+        this.handleEqualSign();
+      }
+      else {
+        this.calculatedValue = 'Error';
+      }
+    }
+
+    //console.log('handleCharacter()');
+  }
+
+  handleOperator(buttonValue: string) {
+    if (this.prevButtonValIsOperator) {
       this.calculatedValue = this.calculatedValue.substring(0, this.calculatedValue.length - 1) + buttonValue;
     }
-    else if (buttonValue === '.') {
-      if (this.prevButtonValIsOperator) {
-        this.calculatedValue += '0.';
-      }
-      else if (!(this.calculatedValue.toString().includes('.'))) {
-        if (this.calculatedValue.length === 0) {
-          this.calculatedValue = '0.';
-        }
-        else {
-          this.calculatedValue += buttonValue;
-        }
-        console.log("calculatedValue doesn't contain a .");
-      }
-      else if (this.previousButton === '.') { return; }
-      else { return; }
-    }
-    else if (buttonValue === '=') {
-      this.calculatedValue = eval(this.calculatedValue);
-    }
-    else if (this.calculatedValue === '0') {
-      if (buttonValue === '0') {
-        this.handleClear();
-      }
-      else if (this.buttonValIsNumber) {
-        this.calculatedValue = buttonValue;
-      }
-      else if (this.buttonValIsCharacter) {
-        this.calculatedValue += buttonValue;
-      }
-    }
-    else if (this.buttonValIsNumber || this.buttonValIsOperator) {
-      console.log("line 118");
+    else {
       this.calculatedValue += buttonValue;
+    }
+
+    this.previousButton = buttonValue;
+    //console.log('handleOperator()');
+  }
+
+  handlePeriod() {
+    if (this.prevButtonValIsOperator) {
+      this.calculatedValue += '0.';
+      this.previousButton = '.';
+    }
+    else if (this.cvHasPeriod) {
+      if (this.cvHasOperator()) {
+        let valid = this.checkValidity();
+        if (valid) {
+          this.calculatedValue += '.';
+          this.previousButton = '.';
+        }
+      }
+
+      //console.log("calculatedValue contains a .");
+    }
+    else if (!(this.cvHasPeriod)) {
+      if (this.calculatedValue.length == 0) {
+        this.calculatedValue = '0.';
+      }
+      else {
+        this.calculatedValue += '.';
+      }
+
+      this.previousButton = '.';
+      //console.log("calculatedValue doesn't contain a .");
+    }
+    else if (this.previousButton === '.') {
+      return;
     }
     else {
       this.calculatedValue = 'Error';
     }
+
+    //console.log('handlePeriod()');
+  }
+
+  handleEqualSign() {
+    this.previousButton = '=';
+
+    if (this.prevButtonValIsOperator) {
+      this.calculatedValue += this.calculatedValue.substring(0, this.calculatedValue.length - 1);
+    }
     
-    this.previousButton = buttonValue;
+    if (isNaN(evaluate(this.calculatedValue.toString()))) {
+      this.calculatedValue = 'Undefined';
+    }
+    else {
+      this.calculatedValue = evaluate(this.calculatedValue.toString());
+    }
+
+    //console.log('handleEqualSign()');
   }
 }
